1cdb5076b7596b28cb8b5a38632f3b94833e0a86
Zach <LE2189542@crc-lennox.qc.ca>
2022-11-28 15:58:03 -0500

    Created routes and added NotFound component

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './Home';
+import Catagory from './Catagory';
+import Quiz from './Quiz';
+import Result from './Result';
+import Leaderboard from './Leaderboard';
+import NotFound from './NotFound';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route index element={<Home/>}/>
+    <Route path="/catagory" element={<Catagory/>}/>
+    <Route path="/quiz" element={<Quiz/>}/>
+    <Route path="/result" element={<Result/>}/>
+    <Route path="/leaderboard" element={<Leaderboard/>}/>
+    <Route path="*" element={<NotFound/>}/>
+  </Routes>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-import React from 'react'
-
-function index() {
-  return (
-    <div>index</div>
-  )
-}
-
-export default index
