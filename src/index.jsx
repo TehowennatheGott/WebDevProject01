d1440fb05a00273aa48490b803b50681447eb524
Zach <LE2189542@crc-lennox.qc.ca>
2022-11-28 20:11:46 -0500

    Added dependencies for react-router-dom to the .json file

--- src/index.jsx
+++ src/index.jsx
@@ -8,20 +8,20 @@ import Result from './Result';
 import Leaderboard from './Leaderboard';
 import NotFound from './NotFound';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <Routes>
-    <Route index element={<Home/>}/>
-    <Route path="/catagory" element={<Catagory/>}/>
-    <Route path="/quiz" element={<Quiz/>}/>
-    <Route path="/result" element={<Result/>}/>
-    <Route path="/leaderboard" element={<Leaderboard/>}/>
-    <Route path="*" element={<NotFound/>}/>
-  </Routes>
-    <App />
+    <Routes>
+      <Route index element={<Home/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/catagory" element={<Catagory/>}/>
+      <Route path="/quiz" element={<Quiz/>}/>
+      <Route path="/result" element={<Result/>}/>
+      <Route path="/leaderboard" element={<Leaderboard/>}/>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
   </BrowserRouter>
 );
 
