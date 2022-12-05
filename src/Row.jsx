70453827e7159abc7e854e71fbb31e4d51fc6882
Zach <LE2189542@crc-lennox.qc.ca>
2022-12-05 12:20:56 -0500

    Added data to rows

--- src/Row.jsx
+++ src/Row.jsx
@@ -6,7 +6,7 @@ function Row(props) {
         <div className= {props.style}>
             <div className="row bg-light" style={{height: "30px"}}>
                 <div className="col border">{props.name}</div>
-                <div className="col border">{props.catagory}</div>
+                <div className="col border">{props.category}</div>
                 <div className="col border">{props.score}</div>
             </div>
         </div>
--- src/leaderboard.jsx
+++ src/leaderboard.jsx
@@ -27,34 +27,35 @@ function Leaderboard() {
 
     const data = await response.json();
     console.log('data: ', data);
-    
+
+    setscores(data);
   }
   return (
     <div>
-      <div className="">
+      <div>
         <div className="container-fluid">
           <div className="row m-0 p-0">
             <div className="col-8 p-0">
               <h1 className="mt-5 pt-5 pb-3 pe-5 text-end">Top Scores</h1>
             </div>
             <div className="col text-end pt-5 mt-3 pe-3">
-              <SquareButton style="" destination="/Category" text="Replay" />
+              <SquareButton destination="/Category" text="Replay"/>
             </div>
           </div>
         </div>
         <br/><br/>
         <div className="container-fluid w-75 text-center">
-          <Row name="Name" catagory="Catagory" score="Score" style="fw-bold"/>
-          <Row name="" catagory="" score=""/>
-          <Row name="" catagory="" score=""/>
-          <Row name="" catagory="" score=""/>
-          <Row name="" catagory="" score=""/>
-          <Row name="" catagory="" score=""/>
-          <Row name="" catagory="" score=""/>
-          <Row name="" catagory="" score=""/>
-          <Row name="" catagory="" score=""/>
-          <Row name="" catagory="" score=""/>
-          <Row name="" catagory="" score=""/>
+          <Row name="Name" category="Category" score="Score" style="fw-bold"/>
+          <Row name={scores[0].name} category={scores[0].categoryName} score={scores[0].score}/>
+          <Row name={scores[1].name} category={scores[1].categoryName} score={scores[1].score}/>
+          <Row name={scores[2].name} category={scores[2].categoryName} score={scores[2].score}/>
+          <Row name={scores[3].name} category={scores[3].categoryName} score={scores[3].score}/>
+          <Row name={scores[4].name} category={scores[4].categoryName} score={scores[4].score}/>
+          <Row name={scores[5].name} category={scores[5].categoryName} score={scores[5].score}/>
+          <Row name={scores[6].name} category={scores[6].categoryName} score={scores[6].score}/>
+          <Row name={scores[7].name} category={scores[7].categoryName} score={scores[7].score}/>
+          <Row name={scores[8].name} category={scores[8].categoryName} score={scores[8].score}/>
+          <Row name={scores[9].name} category={scores[9].categoryName} score={scores[9].score}/>
         </div>
       </div>
     </div>
