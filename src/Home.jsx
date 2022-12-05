b93089fa68a8d71981c0ffddc2492139325243c3
2034254 <le2034254@crc-lennox.qc.ca>
2022-12-05 12:24:38 -0500

    Style changes to Home.jsx.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import SquareButton from './SquareButton'
 
-function Home() {
+function home() {
   return (
-    <div className='text-center'>
-      <h1>Welcome to Quiztacular</h1>
-      <span>Made by: Zacharyah Whitcher and Andrew Sherrer</span>
-      <SquareButton text='Play' destination='/Category'></SquareButton>
+    <div>
+      <div className="flex container text-center pt-5 mt-5">
+        <div className="row align-bottom mt-5">
+          <div className="col">
+            <h1>Welcome to Quiztacular</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col py-4">
+            <span>Made by: Zacharyah Whitcher and Andrew Sherrer</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col py-4">
+            <SquareButton text='Play' destination='/Category'></SquareButton>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Home
+export default home
