9afdd2cd846a9f485009ed81aada4c12fb00dc60
Zach <LE2189542@crc-lennox.qc.ca>
2022-12-12 13:40:39 -0500

    Implemented category selection and provided play pages

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
-export const authorizationToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiWmFjaGFyeWFoIiwic3R1ZGVudElEIjoiMjE4OTU0MiIsImlhdCI6MTY3MDI1NjQzNywiZXhwIjoxNjcyODQ4NDM3fQ.9fvW1XBPa7SA7Y2ec6dmxIEEMGp08xGlCSaVBF_tc0k";
+export const authorizationToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiWmFjaGFyeWFoIiwic3R1ZGVudElEIjoiMjE4OTU0MiIsImlhdCI6MTY3MDg2MDM3NSwiZXhwIjoxNjczNDUyMzc1fQ.pD1avzcD1mfUp6RAydfe6_gC2w5z796kOHY6Ud_grTg";
 
 export const top10route = "https://quiz-leaderboard-58cf6.nn.r.appspot.com/scores/top10";
+
+export const openTDhost = "https://opentdb.com/api.php"
